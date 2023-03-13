7dc62724d1ef38aec3cae83fc131815ec677e7d6
Kronborg <mkronborg7@gmail.com>
2023-03-13 08:35:33 +0000

    lol

---
+++ staticfiles/static/users.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+    const role_button = document.getElementById("role_button");
+    const stats_button = document.getElementById("stats_button");
+
+
+    
+    
+
+})
